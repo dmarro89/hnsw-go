5ee036856fce3a29a250065946179dc1c196a460
Davide Marro <davideraffaele.marro@gmail.com>
2026-09-14 17:07:51 +0200

    perf: dispatch Euclidean distance to platform implementation

--- hnsw/utils.go
+++ hnsw/utils.go
@@ -1,26 +1,5 @@
 package hnsw
 
 func EuclideanDistance(a, b []float32) float32 {
-	var sum0, sum1, sum2, sum3 float32
-	i := 0
-
-	for ; i <= len(a)-4; i += 4 {
-		d0 := a[i] - b[i]
-		d1 := a[i+1] - b[i+1]
-		d2 := a[i+2] - b[i+2]
-		d3 := a[i+3] - b[i+3]
-
-		sum0 += d0 * d0
-		sum1 += d1 * d1
-		sum2 += d2 * d2
-		sum3 += d3 * d3
-	}
-
-	var sum float32
-	for ; i < len(a); i++ {
-		d := a[i] - b[i]
-		sum += d * d
-	}
-
-	return sum + sum0 + sum1 + sum2 + sum3
+	return euclideanDistance(a, b)
 }
