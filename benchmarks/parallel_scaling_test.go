f9804869bbd3e09c1f3d27972502a85e59c43340
Davide Marro <davideraffaele.marro@gmail.com>
2026-09-17 09:19:55 +0200

    bench: fix parallel scaling index construction

--- benchmarks/parallel_scaling_test.go
+++ benchmarks/parallel_scaling_test.go
@@ -27,10 +27,20 @@ func BenchmarkParallelBuildScaling(b *testing.B) {
 				b.ReportAllocs()
 				b.ResetTimer()
 				for i := 0; i < b.N; i++ {
-					idx := hnsw.NewHNSW(16, 200, ef)
+					idx, err := hnsw.NewHNSW(hnsw.Config{
+						M:              16,
+						Mmax:           16,
+						Mmax0:          32,
+						EfConstruction: ef,
+						MaxLevel:       16,
+						DistanceFunc:    hnsw.EuclideanDistance,
+					})
+					if err != nil {
+						b.Fatal(err)
+					}
 					if err := idx.BuildParallel(vectors, hnsw.BulkBuildConfig{
-						Workers: workers,
-						BatchSize: max(32, workers*16),
+						Workers:        workers,
+						BatchSize:      max(64, workers*16),
 						EfConstruction: ef,
 					}); err != nil {
 						b.Fatal(err)
